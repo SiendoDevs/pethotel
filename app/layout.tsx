030525584720547b6abf1e7a3f238f176ff0fc73
joaquin-espinosa733 <juaco.enero9@gmail.com>
2024-02-02 10:43:11 -0300

    modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,11 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from './components/ClientOnly';
 
 const font = Nunito(
   { subsets: ["latin"] }
-  );
+);
 
 export const metadata: Metadata = {
   title: "Airbnb",
@@ -20,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Navbar />
+        </ClientOnly>
         {children}</body>
     </html>
   );
