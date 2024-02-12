93ab8f023a2e55be1d05e84229c4628fa860d77c
unknown <catriel.sl@gmail.com>
2024-02-12 13:30:49 -0300

    Navbar y Register UI

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -2,8 +2,20 @@
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import { useCallback, useState } from "react";
+import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
+
+
 
 const UserMenu = () => {
+    const registerModal = useRegisterModal();
+    const [isOpen, setIsOpen ] = useState(false);
+
+    const toggleOpen = useCallback(() => {
+        setIsOpen((value) => !value);
+    },[]);
+
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
@@ -25,7 +37,7 @@ const UserMenu = () => {
                     Airbnb your home
                 </div>
                 <div
-                    onClick={() => {}}
+                    onClick={toggleOpen}
                     className="
                         p-4
                         md:py-1
@@ -48,6 +60,37 @@ const UserMenu = () => {
                     </div>
                 </div>
             </div>
+
+            {isOpen && (
+                <div
+                    className="
+                        absolute
+                        rounded-xl
+                        shadow-md
+                        w-[40vh]
+                        md:3/4
+                        bg-white
+                        overflow-hidden
+                        right-0
+                        top-12
+                        text-sm
+                    "
+                >
+                    <div className="flex flex-col cursor-pointer">
+                        <>
+                        <MenuItem 
+                            onClick={() => {}}
+                            label="Login"
+                        />
+                         <MenuItem 
+                            onClick={registerModal.onOpen}
+                            label="Sign Up"
+                        />
+                        </>
+                    </div>
+                </div>
+            )}
+
         </div>
     )
 }
